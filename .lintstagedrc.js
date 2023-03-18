00ee07cba97d00dff523dad0f6071f17cecb1046
gxx <1786232520@qq.com>
2023-03-18 23:12:36 +0800

    代码提交

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  'src/**/*.{js,jsx,vue}': ['eslint --ext', 'eslint --fix', 'prettier --write'],
+  'src/**/*.{js,jsx,vue}': ['eslint --fix', 'prettier --write'],
   'src/**/*.{css,less,scss}': ['prettier --write'],
   'src/**/*.{md,json}': ['prettier --write'],
 };
